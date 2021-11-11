c61a8f8ffbebd8297798eea954baac19b0dd7314
menashekoren <menashekoren@gmail.com>
2021-11-11 15:27:06 +0200

    Feat: Initiate popup openings

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,25 @@
 import cousteau from "../images/cousteau.jpg";
 import edit from "../images/edit.svg";
 
-export function Main(props) {
+export function Main() {
+  const handleEditAvatarClick = () => {
+    document
+      .querySelector(".popup_type_edit-avatar")
+      .classList.add("popup_opened");
+  };
+  
+  const handleEditProfileClick = () => {
+    document
+      .querySelector(".popup_type_edit-profile")
+      .classList.add("popup_opened");
+  };
+  
+  const handleAddPlaceClick = () => {
+    document
+      .querySelector(".popup_type_add-card")
+      .classList.add("popup_opened");
+  };
+  
   return (
     <main className="main">
       <section className="profile">
@@ -11,13 +29,19 @@ export function Main(props) {
             src={cousteau}
             alt="Jacques Cousteau smiling on the sea"
           />
-          <img className="avatar__edit-icon" src={edit} alt="Edit icon" />
+          <img
+            onClick={handleEditAvatarClick}
+            className="avatar__edit-icon"
+            src={edit}
+            alt="Edit icon"
+          />
         </div>
         <div className=" profile-info">
           <div className=" profile-info__name">
             <div className=" profile-info__edit">
               <h1 className=" edit-name ellipses">Jacques Cousteau</h1>
               <button
+                onClick={handleEditProfileClick}
                 className=" edit-button button button_hover_dark"
                 type=" button"
               />
@@ -25,6 +49,7 @@ export function Main(props) {
             <p className=" profile-info__profession ellipses">Explorer</p>
           </div>
           <button
+            onClick={handleAddPlaceClick}
             className=" add-button button button_hover_dark"
             type=" button"
           />
@@ -32,7 +57,7 @@ export function Main(props) {
       </section>
       
       <section className=" cards">
-        <ul className=" card-list"></ul>
+        <ul className=" card-list" />
       </section>
     </main>
   );
