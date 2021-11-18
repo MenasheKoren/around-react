cf786619fa81e7b1b0e39eeba91ac981c91108ec
menashekoren <menashekoren@gmail.com>
2021-11-18 11:43:26 +0200

    Feat: TEST map cardList

--- src/components/Main.js
+++ src/components/Main.js
@@ -22,7 +22,7 @@ export function Main(props) {
         setIsUserName(userData.name);
         setIsUserDescription(userData.about);
         setIsUserAvatar(userData.avatar);
-        setIsCardList([...isCardList, cardData.card]);
+        setIsCardList([...cardData]);
         setIsCardTitle([cardData.title]);
         setIsCardLink([cardData.link]);
         setIsLikesCount([cardData.likes].length);
@@ -79,15 +79,31 @@ export function Main(props) {
           />
         </div>
       </section>
+      
       <Card
-        cardList={isCardList}
         cardTitle={isCardTitle}
         cardLink={isCardLink}
         likesCount={isLikesCount}
+        cardList={isCardList}
       >
-        <ul className="card-list">
-          cardList.map(card => {<li className="card" />})
-        </ul>
+        cardList.map(card =>
+        {
+          <li className="card">
+            <button className="card__remove button button_hover_dark" />
+            <img className="card__image" src={cardLink} alt={cardTitle} />
+            <div className="card__caption">
+              <h3 className="card__location ellipses">{cardTitle}</h3>
+              <div className="likes-container">
+                <button
+                  className="card__like button button_empty button_hover_light"
+                  type="button"
+                />
+                <span className="card__likes-count">{likesCount}</span>
+              </div>
+            </div>
+          </li>
+        }
+        )
       </Card>
     </main>
   );
