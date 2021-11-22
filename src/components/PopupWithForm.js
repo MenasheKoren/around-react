232d5be56676807563c13a090a8c7b565f683a4e
menashekoren <menashekoren@gmail.com>
2021-11-22 12:36:59 +0200

    Hotfix: Add form element to PopupWithForm.js

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -6,23 +6,23 @@ export const PopupWithForm = ({
                                 closeAllPopups
                               }) => {
   return (
-    <>
-      <section
-        className={[
-          `popup popup_type_${name}`,
-          isOpen ? "popup_opened" : ""
-        ].join(" ")}
-      >
-        <div className="popup__container">
-          <button
-            className="popup__close button button_hover_dark"
-            type="button"
-            onClick={closeAllPopups}
-          />
-          <h2 className="popup__title">{title}</h2>
-          {children}
-        </div>
-      </section>
-    </>
+    
+    <section
+      className={[
+        `popup popup_type_${name}`,
+        isOpen ? "popup_opened" : ""
+      ].join(" ")}
+    >
+      <div className="popup__container">
+        <button
+          className="popup__close button button_hover_dark"
+          type="button"
+          onClick={closeAllPopups}
+        />
+        <h2 className="popup__title">{title}</h2>
+        <form className="popup__form">{children}</form>
+      </div>
+    </section>
+  
   );
 };
