35c098707a67fba579bf19f46147fef0380d6924
menashekoren <menashekoren@gmail.com>
2021-11-28 20:39:30 +0200

    Feat: Add CurrentUserContext to Card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,27 @@
 import React from "react";
+import { CurrentUserContext } from "../contexts/CurrentUserContext";
 
 function Card(props) {
+  const currentUser = React.useContext(CurrentUserContext);
+
+  // Checking if the current user is the owner of the current card
+  const isOwn = props.card.owner._id === currentUser._id;
+
+  // Creating a variable which you'll then set in `className` for the delete button
+  const cardDeleteButtonClassName = `card__delete-button ${
+    isOwn ? "card__delete-button_visible" : "card__delete-button_hidden"
+  }`;
+
+  // Check if the card was liked by the current user
+  const isLiked = props.card.likes.some((i) => i._id === currentUser._id);
+
+  // Create a variable which you then set in `className` for the like button
+  const cardLikeButtonClassName = `...`;
+
   function handleClick() {
     props.onCardClick(props.card);
   }
-  
+
   return (
     <li className="card">
       <button className="card__remove button button_hover_dark" />
