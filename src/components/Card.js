78a7f36203f50d6daca8ad871ed8b347a263792f
menashekoren <menashekoren@gmail.com>
2021-11-29 14:01:21 +0200

    Feat: Add delete icon only for current user

--- src/components/Card.js
+++ src/components/Card.js
@@ -8,9 +8,9 @@ function Card(props) {
   const isOwn = props.card.owner._id === currentUser._id;
 
   // Creating a variable which you'll then set in `className` for the delete button
-  const cardDeleteButtonClassName = `card__delete-button ${
-    isOwn ? "card__delete-button_visible" : "card__delete-button_hidden"
-  }`;
+  // const cardDeleteButtonClassName = `card__delete-button ${
+  //   isOwn ? "card__delete-button_visible" : "card__delete-button_hidden"
+  // }`;
 
   // Check if the card was liked by the current user
   const isLiked = props.card.likes.some((i) => i._id === currentUser._id);
@@ -24,7 +24,9 @@ function Card(props) {
 
   return (
     <li className="card">
-      <button className="card__remove button button_hover_dark" />
+      {isOwn && (
+        <button className="card__remove button button_hover_dark" />
+      )}
       <img
         className="card__image"
         src={props.card.link}
@@ -38,7 +40,9 @@ function Card(props) {
             className="card__like button button_empty button_hover_light"
             type="button"
           />
-          <span className={cardLikeButtonClassName}>{props.card.likes.length}</span>
+          <span className={cardLikeButtonClassName}>
+            {props.card.likes.length}
+          </span>
         </div>
       </div>
     </li>
